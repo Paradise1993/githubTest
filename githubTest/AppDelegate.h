bb93a31a5722e2cfed377fb924f0713d08f16710
Paradise1993 <liyancheng@liyanchengdeMacBook-Pro.local>
2016-03-29 11:37:04 +0800

    Revert "Revert "Initial Commit""
    This reverts commit 2fa4aca23137524acdaf994583d8ec058b99b77e.

---
+++ githubTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  githubTest
+//
+//  Created by 李颜成 on 16/3/29.
+//  Copyright © 2016年 falcon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
